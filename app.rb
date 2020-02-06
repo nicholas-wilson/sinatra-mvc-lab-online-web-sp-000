89bf1f38e7b9a04f4480e1da53fe838011413f63
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-02-06 10:18:21 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ class App < Sinatra::Base
   end
 
   post '/piglatinize' do
-    @pig_latin = PigLatinizer.new(params[:user_phrase]).piglatinize
+    @pig_latin = PigLatinizer.new.piglatinize(params[:user_phrase])
     erb :pig_latin
   end
 end
