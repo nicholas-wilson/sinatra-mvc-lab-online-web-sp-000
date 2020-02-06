24a26097baa25bd54fd18fcf391eb6dee23debf2
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-02-06 09:39:49 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -9,15 +9,6 @@ class PigLatinizer
     latin_string = ''
     @words.each do |word|
       pig_word = turn_word(word)
-      # letters = word.split('')
-      if word.chr.downcase == 'a' || word.chr.downcase == 'e' || word.chr.downcase == 'o' || word.chr.downcase == 'i' || word.chr.downcase == 'u'
-        pig_word = word + 'way'
-      else
-        split_point = word.index(/[aeiouAEIOU]/)
-        consonant_tag = word.slice(0, split_point)
-        rest_of_word = word.slice(split_point, word.size)
-        pig_word = rest_of_word + consonant_tag + 'ay'
-      end
       latin_string += ' ' + pig_word
     end
     latin_string
