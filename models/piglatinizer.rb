3db7bd2a4bd65a3930c800f7f1bd931b755da56c
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-02-06 09:37:40 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -22,4 +22,18 @@ class PigLatinizer
     end
     latin_string
   end
+
+  def turn_word(word)
+    pig_word = ''
+    # letters = word.split('')
+    if word.chr.downcase == 'a' || word.chr.downcase == 'e' || word.chr.downcase == 'o' || word.chr.downcase == 'i' || word.chr.downcase == 'u'
+      pig_word = word + 'way'
+    else
+      split_point = word.index(/[aeiouAEIOU]/)
+      consonant_tag = word.slice(0, split_point)
+      rest_of_word = word.slice(split_point, word.size)
+      pig_word = rest_of_word + consonant_tag + 'ay'
+    end
+    pig_word
+  end
 end
