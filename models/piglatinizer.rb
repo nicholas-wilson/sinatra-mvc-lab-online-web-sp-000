736dcace277c28b65d29bb05a2cf436bed770ec4
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-02-06 09:25:55 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -13,14 +13,13 @@ class PigLatinizer
       if word.chr.downcase == 'a' || word.chr.downcase == 'e' || word.chr.downcase == 'o' || word.chr.downcase == 'i' || word.chr.downcase == 'u'
         pig_word = word + 'way'
       else
-        stop_at = word.index(/[aeiouAEIOU]/)
+        split_point = word.index(/[aeiouAEIOU]/)
         counter = 0
         while counter < stop_at do
-          consonant_tag += letters[counter]
           counter += 1
         end
-        consonant_tag = word.substr(0, counter - 1)
-        rest_of_word = word.substr(counter, word.size - 1)
+        consonant_tag = word.substr(0, split_point - 1)
+        rest_of_word = word.substr(split_point, word.size - 1)
         pig_word = rest_of_word + consonant_tag + 'ay'
       end
       latin_string += ' ' + pig_word
