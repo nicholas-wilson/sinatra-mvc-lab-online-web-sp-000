273cb6603c35f41e7766302e3acfc3e52c60ff25
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-02-06 09:06:31 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -10,24 +10,24 @@ class PigLatinizer
     @words.each do |word|
       pig_word = ''
       letters = word.split('')
-      if letters[0] == 'a' || letters[0] == 'e' || letters[0] == 'o' || letters[0] == 'i' || letters[0] == 'u'
-        pig_word = word + 'way'
-      else
-        consonant_tag = ''
-        rest_of_word = ''
-        stop_at = letters.index(/[aeiou]/)
-        puts letters
-        counter = 0
-        while counter < stop_at do
-          consonant_tag += letters[counter]
-          counter += 1
-        end
-        counter -= 1
-        counter.times do
-          letters[0].delete
-        end
-      end
-      latin_string += ' ' + word
+      puts letters
+      # if letters[0] == 'a' || letters[0] == 'e' || letters[0] == 'o' || letters[0] == 'i' || letters[0] == 'u'
+      #   pig_word = word + 'way'
+      # else
+      #   consonant_tag = ''
+      #   rest_of_word = ''
+      #   stop_at = letters.index(/[aeiou]/)
+      #   counter = 0
+      #   while counter < stop_at do
+      #     consonant_tag += letters[counter]
+      #     counter += 1
+      #   end
+      #   counter -= 1
+      #   counter.times do
+      #     letters[0].delete
+      #   end
+      # end
+      # latin_string += ' ' + word
     end
     latin_string
   end
