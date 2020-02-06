918ff58db6a8dd6cef4d090bf637c75177190401
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-02-06 08:02:49 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,4 +4,16 @@ class PigLatinizer
   def initialize(text)
     @words = text.split(' ')
   end
+
+  def turn_to_pig_latin
+    latin_words = []
+    @words.each do |word|
+
+    end
+  end
+
+  def self.call
+
+    # return pig latinized text
+  end
 end
